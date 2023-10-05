1ba0bccdf3298a5297676d86f77e23013ca2ee71
Naman-Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2023-10-04 22:47:07 -0400

    Revert "Testing first PR workflow"
    This reverts commit f555ffa4073e79c62aceb8dacc3ab5d2053271ac.

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# this is assignment 02
-
-first_name	last_name	email	password
-john	doe	john.doe@example.com	abc123
-jane	doe	jane.doe@example.com	xyz456
+# webapp
+Student Portal 
--- api/controller/userController.js
+++ api/controller/userController.js
@@ -1,10 +1,9 @@
 const sequelize  = require('../models/index')
 
+
 const check = async (req,res) =>  {
-console.log(req.query,req.body)
-  if (Object.keys(req.query).length > 0 || Object.keys(req.body).length > 0) {
-    return res.status(400).send('Bad Request');
-}
+
+   
      
     try {
        const x = await sequelize.authenticate();
