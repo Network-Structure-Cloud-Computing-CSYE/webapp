da1ccb1f62e632c7ef79d576dbe53e9c773138b0
Naman-Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2023-11-07 22:46:29 -0500

    logger and client

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -3,15 +3,21 @@ const sequelize  = require('../models/index')
 const check = async (req,res) =>  {
 console.log(req.query,req.body)
   if (Object.keys(req.query).length > 0 || Object.keys(req.body).length > 0) {
+    logger.error('Health Check - Bad Request: Unexpected query parameters or body content');
+    client.increment('healthz.badRequest');
     return res.status(400).send('Bad Request');
 }
      
     try {
        const x = await sequelize.authenticate();
       //  console.log(x);
+      logger.info('Health Check - Success');
+      client.increment('healthz.success');
         res.status(200).set('Cache-Control', 'no-cache').end();
       } catch (error) {
         // console.log("ERRRRORRR");
+        logger.error(`Health Check - Service Unavailable: ${error.message}`);
+        client.increment('healthz.serviceUnavailable');
         res.status(503).set('Cache-Control', 'no-cache').end();
       }
 
