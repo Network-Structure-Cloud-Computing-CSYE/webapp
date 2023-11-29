ce85b9ed9d98dc22a142c544d3387d4d457a875c
Naman-Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2023-11-29 03:55:18 -0500

    final submission

--- api/config/dbConfig.js
+++ api/config/dbConfig.js
@@ -4,8 +4,9 @@ require('dotenv').config()
 
 module.exports = {
     HOST: `${process.env.DB_HOST}`,
-    USER: `${process.env.DB_USER}`,
-    PASSWORD: `${process.env.DB_PASS}`,
+    // USER: `${process.env.DB_USER}`,
+    USER: `root`,
+    PASSWORD: `NAM@guj250497`,
     DB: `${process.env.DB_DATABASE}`,
     PORT: `${process.env.DB_PORT}`,
     dialect: 'mysql',
